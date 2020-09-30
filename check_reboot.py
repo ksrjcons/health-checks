46cb27dacdebf9034c8940da92a1289f200b52c7
ksrjcons <72105799+ksrjcons@users.noreply.github.com>
2020-09-30 11:09:16 -0400

    Add a dummy comment to uploaded check_reboot.py
    This is meant to practice commands dealing with remote repos

--- check_reboot.py
+++ check_reboot.py
@@ -3,6 +3,7 @@
 # Change Log
 # 2020-09-21: Add a comment line to see reflected in diff and Git
 # 2020-09-22: Add a second comment line only to revert back later
+# 2020-09-30: Add a dummy comment
 
 import os
 # 2020-09-21: The result will be False as there is reboot scheduled
@@ -11,4 +12,5 @@ def check_reboot():
 	"""Returns True if the computer has a pending reboot."""
 	return os.path.exists("/run/reboot-required")
 
+# Dummy commnent to uploaded file
 print("The status of pending reboot is {}.".format(check_reboot()))
